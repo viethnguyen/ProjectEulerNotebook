49716f1b2d600813fecef5342b3545b13f4c7735
Viet H Nguyen <viet.nguyen182@gmail.com>
2013-05-31 11:33:47 +0700

    Initial commit

---
+++ src/edu/rutgers/vietnguyen/AnswerUtilites.java
@@ -0,0 +1,15 @@
+package edu.rutgers.vietnguyen;
+
+public class AnswerUtilites {
+	private final String answerFileName = "Answer.txt";
+	
+	public static void writeAnswer(int probId, int answer)
+	{
+		
+	}
+	
+	public static boolean isAnswerExisted(int probId)
+	{
+		return false;
+	}
+}
