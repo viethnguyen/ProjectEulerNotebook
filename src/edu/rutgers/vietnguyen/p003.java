de146659b8968b2f71f5b023572a928aa932ab17
Viet H Nguyen <viet.nguyen182@gmail.com>
2013-06-01 17:06:55 +0700

    add new solution to problem 3

--- src/edu/rutgers/vietnguyen/p003.java
+++ src/edu/rutgers/vietnguyen/p003.java
@@ -11,11 +11,58 @@ public final class p003  extends EulerSolution{
 	
 	@Override
 	public void run() {
-		probAns = largestPrimeFactor(Long.parseLong("600851475143"));
+		//probAns = largestPrimeFactor(Long.parseLong("600851475143"));
+		probAns = largestPrimeFactor_v2(Long.parseLong("600851475143"));
 		System.out.println(probAns);
 		AnswerUtilities.writeAnswer(probId, probAns);
 	}
 
+	//Version 2: run faster ! 
+	private int largestPrimeFactor_v2(long number)
+	{
+		int factor;
+		int lastFactor;
+		long maxFactor;
+		if( number % 2 == 0)
+		{
+			lastFactor = 2;
+			number = number/2;
+			while(number % 2 == 0)
+				number = number / 2;
+		}
+		else 
+		{
+			lastFactor = 1;
+		}
+		
+		factor = 3;
+		maxFactor = Math.round(Math.sqrt(number));
+		while (number > 1 && factor <= maxFactor)
+		{
+			if(number % factor == 0)
+			{
+				number = number / factor;
+				lastFactor = factor;
+				while(number % factor == 0)
+				{
+					number = number / factor;
+				}
+				maxFactor = Math.round(Math.sqrt(number));
+			}
+			factor += 2;
+		}
+		
+		if(number == 1)
+		{
+			return (int) lastFactor;
+		}
+		else 
+		{
+			return (int) number;
+		}
+	}
+	
+	//Version 1 : Too slow !
 	private int largestPrimeFactor(long number)
 	{
 		int res = 1;
